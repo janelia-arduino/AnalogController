f713e969252317bc20f026087f22f184336b4c08
Peter Polidoro <peterpolidoro@gmail.com>
2021-09-16 12:52:30 -0400

    Update email address

--- src/AnalogController.h
+++ src/AnalogController.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 #ifndef ANALOG_CONTROLLER_H
 #define ANALOG_CONTROLLER_H
